4411b99d122cba4072dae47e16455e5b9fd454f5
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-08-29 23:18:28 +0900

    Cut domain name down

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      "https://github-readme-stats.vercel.app/api/pin/?username=mxngls&repo=Gmail-Cleaner",
-      "https://github-readme-stats.vercel.app/api/pin/?username=mxngls&repo=kled-server",
-      "https://github-readme-stats.vercel.app/api/pin/?username=mxngls&repo=kled-scraper",
-    ],
+    domains: ["github-readme-stats.vercel.app"],
   },
 };
 
