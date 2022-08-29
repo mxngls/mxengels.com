ee1b85d1c5045ae8d546a853234e06d44f242f25
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-08-29 23:14:19 +0900

    Properly include all domains for used images

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['github-readme-stats.vercel.app'],
+    domains: [
+      "https://github-readme-stats.vercel.app/api/pin/?username=mxngls&repo=Gmail-Cleaner",
+      "https://github-readme-stats.vercel.app/api/pin/?username=mxngls&repo=kled-server",
+      "https://github-readme-stats.vercel.app/api/pin/?username=mxngls&repo=kled-scraper",
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
