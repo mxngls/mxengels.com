7efa613c3b5a8bd39b82d97dc08d4769c14f3aa1
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-08-29 23:09:21 +0900

    Properly setup the font-family & format file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                body: "ModernEra"
-            },
-            animation: {
-                shake: "shake linear 1s",
-                scale: "scale linear 1s",
-            },
-            keyframes: {
-                shake: {
-                    "10%, 90%": { transform: "rotate(0.03turn)" },
-                    "20%, 80%": { transform: "rotate(-0.01turn)" },
-                    "30%, 50%, 70%": { transform: "rotate(0.03turn)" },
-                    "40%, 60%": { transform: "rotate(-0.04turn)" },
-                },
-                scale: { "20%, 70%": { transform: "scale(115%)" } },
-            },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+      animation: {
+        shake: "shake linear 3s",
+        scale: "scale linear 3s",
+      },
+      keyframes: {
+        shake: {
+          "10%, 90%": { transform: "rotate(0.03turn)" },
+          "20%, 80%": { transform: "rotate(-0.01turn)" },
+          "30%, 50%, 70%": { transform: "rotate(0.03turn)" },
+          "40%, 60%": { transform: "rotate(-0.04turn)" },
         },
+        scale: { "20%, 70%": { transform: "scale(115%)" } },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
