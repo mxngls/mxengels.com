3ed2c7f0c69e6aae1b3a3bd05bdd05b0032aedd9
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-08-31 15:16:21 +0900

    Implement context consumer via useContex() hook

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { ContextType, ReactNode, useContext, useState } from "react";
+import { LanguageContext } from "./LanguageContext";
 import Navbar from "./Navbar";
 import NavElement from "./NavElement";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { code, toggleLanguage } = useContext(LanguageContext);
+
   return (
-    <div>
+    <>
       <header>
         <Navbar>
           <NavElement reference={"/"} content={"Home"} />
           <NavElement reference={"/projects"} content={"Projects"} />
+          <button className="ml-auto" onClick={() => toggleLanguage()}>
+            <a>{code === "en" ? "한국어" : "English"}</a>
+          </button>
         </Navbar>
       </header>
       <main className="flex flex-col">{children}</main>
-    </div>
+      <button />
+    </>
   );
 }
