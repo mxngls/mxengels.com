53becc686b5c03010fb7e78bda01f5c6c6f00afe
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-08-31 15:17:45 +0900

    Implement context at the MyApp()

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import { AppProps } from 'next/app';
-import '../styles/globals.css'
+import React, { useState } from "react";
+import { AppProps } from "next/app";
+import { LanguageContext } from "../components/LanguageContext";
+import "../styles/globals.css";
+
+import EnglishContent from "../public/en.json";
+import KoreanContent from "../public/kr.json";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [language, setLanguage] = useState({
+    code: "en",
+    content: EnglishContent,
+  });
+
+  const toggleLanguage = () => {
+    if (language.code === "en") {
+      setLanguage((language) => ({
+        ...language,
+        code: "kr",
+        content: KoreanContent,
+      }));
+    } else {
+      setLanguage((language) => ({
+        ...language,
+        code: "en",
+        content: EnglishContent,
+      }));
+    }
+  };
+  return (
+    <LanguageContext.Provider
+      value={{
+        code: language.code,
+        content: language.content,
+        toggleLanguage: toggleLanguage,
+      }}
+    >
+      <Component {...pageProps} />
+    </LanguageContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
